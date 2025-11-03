6526c13f08980ce6a71fd773547a149b4d32ef03
priyamvad-tripathi <64955194+priyamvad-tripathi@users.noreply.github.com>
2025-11-03 16:44:40 +0000

    renamed and added multi model script

--- tests/deblending.py
+++ tests/deblending.py
@@ -1,9 +1,8 @@
 # %%Import Libraries
-import torch
 from torch.utils.data import DataLoader
 
 from deepshape2.data import loaders
-from deepshape2.deblending import plot_bad_cases, predict
+from deepshape2.deblending import predict_multiple
 from deepshape2.models import VAE
 from deepshape2.utils import get_freest_gpu, load_config, set_seed
 
@@ -14,11 +13,17 @@ DATA_DIR = cfg["DATA_DIR"]
 beta = cfg["VAE_beta"]
 lr_init = cfg["VAE_lr_init"]
 
-loc_weights = cfg["MODEL_DIR"] + "vae_deblender.pt"
+loc_weights = cfg["MODEL_DIR"]
 
 # Torch Parameters
 device = get_freest_gpu(set_device=True)
 set_seed()
+
+
+# Load the models
+model1 = VAE()
+model2 = VAE(attention=False)
+
 
 # %% Load Data and Model
 
@@ -39,14 +44,10 @@ test_loader = DataLoader(
     pin_memory=True,
 )
 
-# Load the VAE model
-model = VAE()
-model = model.to(device)
 
-# %% Load Model Weights
-checkpoint = torch.load(loc_weights, map_location=device, weights_only=False)
-metrics = predict(model, checkpoint["best_weights"], test_loader, device)
-
-# %% Plot the worst cases
-plot_bad_cases(metrics, category="input")
-plot_bad_cases(metrics, category="output")
+results = predict_multiple(
+    [model1, model2],
+    [loc_weights + "vae_deblender.pt", loc_weights + "simple_deblender.pt"],
+    test_loader,
+    device,
+)

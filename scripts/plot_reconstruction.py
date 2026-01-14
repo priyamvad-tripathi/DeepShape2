da0f8cf06633e85550c1876ff7845caebe93e7a2
priyamvad-tripathi <64955194+priyamvad-tripathi@users.noreply.github.com>
2026-01-14 18:55:28 +0000

    Added new reconstruction plots with isolated reconstructions

--- scripts/plot_reconstruction.py
+++ scripts/plot_reconstruction.py
@@ -1,7 +1,9 @@
 # %% Imports
 import numpy as np
 import pandas as pd
+from ska_sdp_datamodels.visibility import create_visibility_from_ms
 
+from deepshape2.reconstruction import residual_facet_image
 from deepshape2.utils import extract_image, load_config, load_h5
 from deepshape2.visualization import metric_dependence, plot
 
@@ -16,6 +18,7 @@ patch_df = pd.DataFrame.from_records(patch_group["patch_df"][()])
 
 mask_flux = patch_df["flux_mask"].values
 flux = patch_df["flux"].values[mask_flux]
+galaxy_locations = patch_df[["pix_x", "pix_y"]].to_numpy()[mask_flux]
 
 iso_images = extract_image(patch_group["isolated_stamps"][:])
 blend_images = extract_image(patch_group["blended_stamps"][:])
@@ -34,7 +37,12 @@ psnr_iso = patch_group["isolated_dirty_psf/psnr"][:]
 shape_diff_iso = patch_group["isolated_dirty_psf/shape_diff"][:]
 
 # %% Example indices to plot
-inds = [11816, 12782, 6412, 14023]
+inds = [11816, 1301, 5684, 14023]
+
+vis = create_visibility_from_ms(DATA_DIR + "MS/vis_deep_set_patch_000.ms")[0]
+residuals = np.array(
+    [residual_facet_image(vis, decon_blend[i], galaxy_locations[i]) for i in inds]
+)
 
 # Prepare metric labels for display
 metrics_labels = [
@@ -51,13 +59,14 @@ images_to_plot = [
     blend_images[inds],
     dirty_facet[inds],
     decon_blend[inds],
-    # residuals can be recomputed here if needed
+    residuals,
     recon_blend[inds],
 ]
 
 subtitles_list = [
     flux_labels,
     blendedness_labels,
+    none_titles,
     none_titles,
     none_titles,
     metrics_labels,
@@ -70,48 +79,52 @@ scales = plot(
         "Blended",
         "Dirty",
         "Deconvolved",
+        "Residual",
         "Reconstructed",
     ],
     cbar=True,
     subtitles=subtitles_list,
-    same_scale=[0, 1, 3, 4],
+    same_scale=[0, 1, 3, 5],
     scale_row=0,
     return_scales=True,
     fname=RESULTS_DIR + "reconstruction/stamps.pdf",
 )
 # %%
-mask = (blendedness_vals > 0.2) & (flux > 40 * 1e-6)
+# mask = (blendedness_vals > 0.1) & (flux > 30 * 1e-6)
 
-# Indices that satisfy the condition
-valid_indices = np.where(mask)[0]
+# # Indices that satisfy the condition
+# valid_indices = np.where(mask)[0]
 
-# Randomly choose 10 (or fewer if not enough)
-num_to_select = min(10, len(valid_indices))
-inds = np.random.choice(valid_indices, size=num_to_select, replace=False)
+# # Randomly choose 10 (or fewer if not enough)
+# num_to_select = min(10, len(valid_indices))
+# inds = np.random.choice(valid_indices, size=num_to_select, replace=False)
 
-flux_labels = [f"{f * 1e6:.3f} µJy" for f in flux[inds]]
-blendedness_labels = [f"{b:.3f}" for b in blendedness_vals[inds]]
-metrics1 = [
-    f"{p:.02f} dB / {sd:.03f}"
-    for p, sd in zip(psnr_blend[inds], shape_diff_blend[inds])
-]
-metrics_2 = [
-    f"{p:.02f} dB / {sd:.03f}" for p, sd in zip(psnr_iso[inds], shape_diff_iso[inds])
-]
-subs = [flux_labels, blendedness_labels, metrics1, metrics_2]
-print("Selected indices:", inds)
-plot(
-    [
-        iso_images[inds],
-        blend_images[inds],
-        recon_blend[inds],
-        recon_iso[inds],
-    ],
-    cbar=True,
-    same_scale=[0, 1, 2, 3],
-    scale_row=0,
-    subtitles=subs,
-)
+# flux_labels = [f"{f * 1e6:.3f} µJy" for f in flux[inds]]
+# blendedness_labels = [f"{b:.3f}" for b in blendedness_vals[inds]]
+# metrics1 = [
+#     f"{p:.02f} dB / {sd:.03f}"
+#     for p, sd in zip(psnr_blend[inds], shape_diff_blend[inds])
+# ]
+# metrics_2 = [
+#     f"{p:.02f} dB / {sd:.03f}" for p, sd in zip(psnr_iso[inds], shape_diff_iso[inds])
+# ]
+
+
+# subs = [flux_labels, blendedness_labels, [None] * len(inds), metrics1, metrics_2]
+# print("Selected indices:", inds)
+# plot(
+#     [
+#         iso_images[inds],
+#         blend_images[inds],
+#         decon_blend[inds],
+#         recon_blend[inds],
+#         recon_iso[inds],
+#     ],
+#     cbar=True,
+#     same_scale=[0, 1, 2, 3, 4],
+#     scale_row=0,
+#     subtitles=subs,
+# )
 
 # %% Plot isolated reconstruction for comparison
 images2 = [

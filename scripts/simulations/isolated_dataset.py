4a0da7206e3a90056fd50d873ff54a881f0ae9fb
priyamvad-tripathi <64955194+priyamvad-tripathi@users.noreply.github.com>
2026-01-19 15:57:49 +0000

    Created script to simulated isolated stamps for galaxies in DS

--- scripts/simulations/isolated_dataset.py
+++ scripts/simulations/isolated_dataset.py
@@ -9,7 +9,7 @@ from dask import compute, delayed
 from dask.distributed import Client, LocalCluster
 
 from deepshape2.simulation import make_dirty_image_and_psf, simulate_visibilities
-from deepshape2.utils import extract_image, load_config, load_h5, post_step, save
+from deepshape2.utils import extract_image, load_config, load_h5, post_step
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -23,37 +23,16 @@ NPIX_SKY = cfg["NPIX_SKY"]
 SCALE_RADIANS = cfg["SCALE_RADIANS"]
 
 
-# %% Functions
-def simulate_batch(inds):
-    isolated_stamps_batch = isolated_stamps[inds]
-    centres = galaxy_centres[inds]
-
-    dirty_batch = np.zeros_like(isolated_stamps_batch)
-    psf_batch = np.zeros_like(isolated_stamps_batch)
-
-    for i, (stamp, centre) in enumerate(zip(isolated_stamps_batch, centres)):
-        vis = simulate_visibilities(
-            field=stamp,
-            ra_pointing=centre.ra.deg,
-            dec_pointing=centre.dec.deg,
-            create_dirty=False,
-        )
-
-        dirty, psf = make_dirty_image_and_psf(vis)
-        dirty_batch[i] = dirty
-        psf_batch[i] = psf
-
-    return dirty_batch, psf_batch
-
-
 # %%
 
 if __name__ == "__main__":
     start = time.time()
-    hf = load_h5(hf_path, "r", delete_if_exists=False)
-    patch_group = hf["patch_000"]
-    isolated_stamps = extract_image(patch_group["isolated_stamps"][:])
 
+    # Open HDF5 file in append mode
+    hf = load_h5(hf_path, "a", delete_if_exists=False)
+    patch_group = hf["patch_000"]
+
+    isolated_stamps = extract_image(patch_group["isolated_stamps"][:])
     patch_df = patch_group["patch_df"][()]
     patch_ra, patch_dec = patch_group.attrs["centre"]
 
@@ -76,6 +55,34 @@ if __name__ == "__main__":
 
     galaxy_centres = phase_centre.spherical_offsets_by(offset_ra, offset_dec)
 
+    n_total = len(isolated_stamps)
+    stamp_shape = isolated_stamps.shape[1:]
+
+    # Output group
+    group_name = "isolated_dirty_psf"
+    if group_name in patch_group:
+        del patch_group[group_name]
+
+    out_group = patch_group.create_group(group_name)
+
+    # Preallocate datasets
+    batch_size = 500
+    dirty_ds = out_group.create_dataset(
+        "dirty",
+        shape=(n_total, *stamp_shape),
+        dtype=isolated_stamps.dtype,
+        chunks=(batch_size, *stamp_shape),
+        maxshape=(n_total, *stamp_shape),
+    )
+    psf_ds = out_group.create_dataset(
+        "psf",
+        shape=(n_total, *stamp_shape),
+        dtype=isolated_stamps.dtype,
+        chunks=(batch_size, *stamp_shape),
+        maxshape=(n_total, *stamp_shape),
+    )
+
+    # Start Dask cluster
     with (
         LocalCluster(
             n_workers=64,
@@ -88,21 +95,37 @@ if __name__ == "__main__":
     ):
         print("Dask dashboard:", client.dashboard_link)
 
-        chunk_size = 100
-        n = len(isolated_stamps)
+        for start_idx in range(0, n_total, batch_size):
+            inds = np.arange(start_idx, min(start_idx + batch_size, n_total))
 
-        index_chunks = [
-            np.arange(i, min(i + chunk_size, n)) for i in range(0, n, chunk_size)
-        ]
+            # create chained delayed tasks
+            delayed_dirty_psf = [
+                delayed(make_dirty_image_and_psf)(
+                    delayed(simulate_visibilities)(
+                        field=isolated_stamps[i],
+                        ra_pointing=galaxy_centres[i].ra.deg,
+                        dec_pointing=galaxy_centres[i].dec.deg,
+                        create_dirty=False,
+                    )
+                )
+                for i in inds
+            ]
 
-        tasks = [delayed(simulate_batch)(inds) for inds in index_chunks]
+            # Step 3: compute batch
+            results = compute(*delayed_dirty_psf)
 
-        results = compute(*tasks)
+            # Step 4: write to HDF5
+            for idx, (dirty, psf) in zip(inds, results):
+                dirty_ds[idx] = dirty
+                psf_ds[idx] = psf
 
-        dirty_all = np.concatenate([r[0] for r in results], axis=0)
-        psf_all = np.concatenate([r[1] for r in results], axis=0)
+            post_step(
+                f"Processed stamps {start_idx}–{start_idx + len(inds)} / {n_total}",
+                start,
+                data=hf,
+                client=client,
+            )
 
-        post_step("Simulated dirty images and PSFs", start, data=hf)
-
-        data = {"dirty": dirty_all, "psf": psf_all}
-        save(data, DATA_DIR + "isolated_dirty_psf_deep_set.pkl")
+    # Final checkpoint
+    post_step("Simulated dirty images and PSFs", start, data=hf)
+    hf.close()
